4220d6af93a5fdfe054e36ce995db8630791800b
CT08\CT08 <pcthanh0802@gmail.com>
2022-12-15 11:16:41 +0700

    factor code folder

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+// middlewares
+app.use(morgan('combined'));
+app.use(helmet());
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+
+// start up the server
+const port = process.env.PORT || 3281;
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}...`);
+})
