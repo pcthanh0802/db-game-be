a15ef5ab4ad8764b3d80b93af6cfbf74a1ff3257
CT08\CT08 <pcthanh0802@gmail.com>
2022-12-16 08:08:24 +0700

    fix dlc api

--- api/dlc.js
+++ api/dlc.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const Dlc = require('../controllers/dlcApi');
 
 router.get('/', Dlc.getDlcsOfGame);
-router.get(':id', Dlc.getDlc);
+router.get('/:id', Dlc.getDlc);
 
 module.exports = router;
